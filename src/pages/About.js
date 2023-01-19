d698e6984eb29781385f44f5ca39dddb712552d8
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-19 13:53:53 -0500

    initial project setup updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,8 @@
 const About = () => {
   return (
-    <section className='section'>
+    <section className="section">
       <h2>About</h2>
     </section>
-  );
-};
-export default About;
+  )
+}
+export default About
