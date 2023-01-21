af27604a2326035299a1f51f0fc32b1ca4dcc9c7
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-21 06:08:14 -0500

    passing data from products page to SingleProductPage implemented

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom"
+import products from "../data"
+
 const Products = () => {
   return (
     <>
       <section className="section">
         <h2>Products</h2>
+        <div className="products">
+          {products.map(product => {
+            const { id, name, image } = product
+            return (
+              <article key={id}>
+                <h4>{name}</h4>
+                <Link to={`/products/${id}`}>More Info</Link>
+              </article>
+            )
+          })}
+        </div>
       </section>
     </>
   )
